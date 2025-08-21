345363d2b0a5cada643dbe9f2c62e40c8f97de37
ShreyasTantri <shreyastantrik29@gmail.com>
2025-08-21 15:15:02 +0530

    Added Colors and frames

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -16,10 +16,29 @@ struct ContentView: View {
         //            Text("Third")
         //        }
         //    }
-        ZStack(alignment: .top) {
-            Text("Hello, world!")
-            Text("This is inside a stack")
+//        ZStack(alignment: .top) {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+//        ZStack {
+//            Color(red: 1, green: 0.8, blue: 0)       // this is a view in itself
+////                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+//            Text("GuessTheFlag")
+////                .background(.red)
+//        }
+//        .ignoresSafeArea()
+//        .background(.red)
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            Text("GuessTheFlag")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
+                .ignoresSafeArea()
     }
 }
 
