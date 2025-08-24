f2eb59d7e0051d85e63f6023cbaaecf2ff58d9e4
ShreyasTantri <shreyastantrik29@gmail.com>
2025-08-24 13:12:42 +0530

    P3: Make a FlagButton view that takes an image name and applies the capsule + shadow style. Use it in your ForEach.P4: Change the alert to show “Corrector “Wrong, that’s the flag of ..

--- PracticingFlagGuessing/PracticingFlagGuessing/ContentView.swift
+++ PracticingFlagGuessing/PracticingFlagGuessing/ContentView.swift
@@ -38,30 +38,54 @@
 //    ContentView()
 //}
 
-// P2: Make a FlagButton view that takes an image name and applies the capsule + shadow style. Use it in your ForEach.
+// P3: Make a FlagButton view that takes an image name and applies the capsule + shadow style. Use it in your ForEach.
+//P4: Change the alert to show “Correct!” or “Wrong, that’s the flag of …”.
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var = 0     Is this the way?? Add name please
+    @State private var score = 0
+    @State private var countries = ["Estonia", "France", "Germany"]
+    @State private var correctAnswer = 1    // index of the correct flag
+    @State private var showAlert = false
+    @State private var alertTitle = ""
+    
     var body: some View {
         ZStack {
             LinearGradient(colors: [.green, .yellow], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
+            Text("Score: \(score)")
+                .font(.largeTitle)
+                .padding()
+            
             VStack {
-//                Button {   // simple Syntax
-//                    print("Hi")
-//                } label: {
-//                    Text("Action")
-//                }
-                Button {
-                    print("Flag tapped")
-                } label: {
-                    Image("Estonia")
-                        .clipShape(Capsule())
-                        .shadow(radius: 5)
+                ForEach(0..<countries.count, id: \.self) { index in
+                    Button {
+                        flagTapped(index)
+                    } label: {
+                        Image(countries[index])
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
+                    }
+                    .alert(alertTitle, isPresented: $showAlert) {
+                        // alert
+                        Button("OK") {
+                            // shuffle flags
+                        }
+                    }
                 }
             }
         }
+    }
+    func flagTapped(_ index: Int) {
+        if index == correctAnswer {
+            score += 1
+            alertTitle = "Correct"
+        } else {
+            alertTitle = "Wrong! That's the flag of \(countries[index])"
+        }
+        showAlert = true
     }
 }
 
