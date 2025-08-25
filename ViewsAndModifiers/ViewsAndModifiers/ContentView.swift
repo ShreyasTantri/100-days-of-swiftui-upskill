9669b15d625257e5ac878e17270c9b53f6b4a35b
ShreyasTantri <shreyastantrik29@gmail.com>
2025-08-25 18:41:29 +0530

    Added swift file ViewsAndModifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by CCS038 on 25/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by CCS038 on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
